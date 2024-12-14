071a57fa7ff71c26e13c5f25074689604dbfdb8c
Aidan Ryan <aryan3@stevens.edu>
2024-12-14 01:20:09 -0500

    database is populated and select * is generated for iteration of the database

--- main.py
+++ main.py
@@ -3,33 +3,34 @@ import psycopg2
 from dotenv import load_dotenv
 
 
-#connect to database
-load_dotenv()
-conn = psycopg2.connect(
-        dbname=os.getenv('DBNAME'),
-        user=os.getenv('USER'),
-        password=os.getenv('PASSWORD'),
-        host=os.getenv('HOST'),
-)
-cur = conn.cursor()
+#function to connect to database and populate it
+def connectAndPopulate():
+    load_dotenv()
+    conn = psycopg2.connect(
+            dbname=os.getenv('DBNAME'),
+            user=os.getenv('USER'),
+            password=os.getenv('PASSWORD'),
+            host=os.getenv('HOST'),
+    )
+    cur = conn.cursor()
+    sales = open("sales.sql", "r")
+    for i in sales:
+        cur.execute(i)
 
-#populate the database
-#this needs to be done but simple, copy from canvas
-
-#get user input for the phi operator info
-userInput = input("Press enter for inline commands or write the name of the file you want to be read: ")
 def parse_file(file_path):
     #for when the user wants to use a file
     with open(file_path, "r", encoding="utf-8") as file:
         lines = file.readlines()
     
-    #iterate the lines
+    
     groupingVars = ""
     selectAttributes = ""
     groupingAttributes = ""
     aggregates = ""
     groupingPredicates = ""
     havingCondition = ""
+    #  iterate the lines of the input file, should be in the same order all the time 
+    # since prof said we don't need to do error checking
     i = 0
     while i < len(lines):
         if lines[i] == "SELECT ATTRIBUTE(S):\n":
@@ -57,6 +58,11 @@ def parse_file(file_path):
     
     return selectAttributes, groupingVars, groupingAttributes, aggregates, groupingPredicates, havingCondition
 
+#call the function to connect to the database and populate it
+connectAndPopulate()
+
+#get user input for the phi operator info
+userInput = input("Press enter for inline commands or write the name of the file you want to be read: ")
 if userInput == "":
     #for when the user wants to use the command line
     selectAttributes  = input("Please input the select attributes seperated by a comma. For example: product, sum_x_quant: ").replace(" ", "")
@@ -74,12 +80,21 @@ with open("_generated.py", "w") as file:
     #write the code to the file to run the query 
     #do the imports and call the generator function in the generator file
     #pretty sure we need to do a select * from sales to do the query
+    #imports
     file.write("import os\n")
     file.write("import psycopg2\n")
-
-# print("Select Attributes: ", selectAttributes)
-# print("Grouping Variables: ", groupingVars)
-# print("Grouping Attributes: ", groupingAttributes)
-# print("Aggerates: ", aggregates)
-# print("Predicates: ", groupingPredicates)
-# print("Having Condition: ", havingCondition)
+    file.write("from dotenv import load_dotenv\n")
+    #run select * from sales so we can iterate over the data
+    file.write("load_dotenv()\n")
+    file.write("conn = psycopg2.connect(\n")
+    file.write("dbname=os.getenv('DBNAME'),\n")
+    file.write("user=os.getenv('USER'),\n")
+    file.write("password=os.getenv('PASSWORD'),\n")
+    file.write("host=os.getenv('HOST'),\n")
+    file.write(")\n")
+    file.write("cur = conn.cursor()\n")
+    file.write("cur.execute(\"SELECT * FROM sales\")\n")
+    file.write("sales = cur.fetchall()\n")
+    file.write("cur.close()\n")
+        
+    
